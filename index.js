f7d2ac586eda6095bff54dc44beb2bf26024ade3
unknown <jose-lucassilva@hotmail.com>
2019-01-30 13:39:19 -0200

    Creating a simple REST API

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const hostname = "127.0.0.1";
+const hostname = "localhost";
 const port = 3000;
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+// WHEN I CALL ONE OF THESE HTTP METHODS, FIRST WILL BE EXECUTED
+// THE APP.ALL METHOD, THEN THE NEXT FUNCTION WILL CALL THE RESPECTIVE
+// METHOD ACCORDING WITH SPECIFC HTTP METHOD
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/plain");
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + 
+    ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation is not supported on /dishes/' +
+    req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+    ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+
 app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
